55a6ae894dfaf88be31a7780ef95d27349278300
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2024-10-05 11:01:57 +0300

    responsiveness

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -15,14 +15,14 @@ const LandingPage = () => {
     return (
       <div className="h-full flex-grow text-white flex flex-col items-center justify-center space-y-8">
         <h1 className="text-4xl font-bold flex items-center"><img src="/logo.svg" className="h-11" alt="Flowbite Logo" />EarthPulse</h1>
-        <p className="text-xl">Explore Earth's climate history through time</p>
-        <div className="flex flex-row">
+        <p className="text-xl text-center">Explore Earth's climate history through time</p>
+        <div className="flex flex-row max-md:flex-col">
         <button
           onClick={() => {
             setLevel(1)
             setPage(1)
           }}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center mx-2"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center mx-2 max-md:m-2"
         >
           Start New Journey <ChevronRight className="ml-2" />
         </button>
